a43192b08990b8ed97d5413173bba84df16651f7
Joao Matheus <j.matheusr@hotmail.com>
2024-07-24 20:12:51 -0300

    Ensuring that my request and completion with error invokes invalid cases correctly.

--- HelpDeskTests/Network/NetworkClientRequestUseCases.swift
+++ HelpDeskTests/Network/NetworkClientRequestUseCases.swift
@@ -11,9 +11,57 @@ import XCTest
 final class NetworkClientRequestUseCases: XCTestCase {
 
     func test_request_resume_dataTask_with_url() {
-        let task = assert()
+        let (sut, session) = makeSUT()
+        let url = URL(string: "https://localhost:3000/")!
+        let task = URLSessionDataTaskSpy()
+        
+        session.stub(url: url, task: task)
+        sut.request(from: url) { _ in }
+        
         XCTAssertEqual(task.resumeCount, 1)
     }
+    
+    func test_request_and_completion_with_error_for_invalidCases() {
+        let url = URL(string: "https://localhost:3000/")!
+        let anyError = NSError(domain: "Any error", code: -1)
+        let data = Data()
+        let httpResponse = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        let urlResponse = URLResponse(url: url, mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        
+        XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: urlResponse, error: nil))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: httpResponse, error: nil))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: nil, error: anyError))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: urlResponse, error: anyError))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: nil, response: httpResponse, error: anyError))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: urlResponse, error: anyError))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: httpResponse, error: anyError))
+        XCTAssertNotNil(resultErrorForInvalidCases(data: data, response: urlResponse, error: nil))
+        
+        let result = resultErrorForInvalidCases(data: nil, response: nil, error: anyError)
+        XCTAssertEqual(result as? NSError, anyError)
+    }
+    
+    private func resultErrorForInvalidCases(
+        data: Data?,
+        response: URLResponse?,
+        error: Error?,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> Error? {
+        
+        let result = assert(data: data, response: response, error: error)
+        
+        switch result {
+        case let .failure(error):
+            return error
+        default:
+            XCTFail("expected failure but returned \(String(describing: result))", file: file, line: line)
+        }
+        return nil
+    }
+
     
     private func makeSUT() -> (NetworkService, URLSessionSpy) {
         let session = URLSessionSpy()
@@ -21,15 +69,28 @@ final class NetworkClientRequestUseCases: XCTestCase {
         return (sut, session)
     }
 
-    private func assert() -> URLSessionDataTaskSpy {
-        let (sut, session) = makeSUT()
-        let url = URL(string: "https://localhost:3000/")!
-        let task = URLSessionDataTaskSpy()
-        session.stub(url: url, task: task)
-        sut.request(from: url) { _ in }
-        
-        return task
-    }
+    private func assert(
+            data: Data?,
+            response: URLResponse?,
+            error: Error?,
+            file: StaticString = #filePath,
+            line: UInt = #line
+        ) -> NetworkService.NetworkResult? {
+            let (sut, session) = makeSUT()
+            let url = URL(string: "https://localhost:3000/")!
+            let task = URLSessionDataTaskSpy()
+
+            let exp = expectation(description: "waiting")
+            var returnedResult: NetworkService.NetworkResult?
+
+            session.stub(url: url, task: task, error: error, data: data, response: response)
+            sut.request(from: url) { result in
+                returnedResult = result
+                exp.fulfill()
+            }
+            wait(for: [exp], timeout: 1.0)
+            return returnedResult
+        }
 }
 
 final class URLSessionSpy: URLSession {
